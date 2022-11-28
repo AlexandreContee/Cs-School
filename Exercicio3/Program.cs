acc43ddbd7c7169313868aa7e4e38dbd0a4f67a7
Alexandre Conte <alexandre.conte10@gmail.com>
2022-11-28 12:17:25 -0300

    Final version exercise

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno: ");
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if(aluno.NotaFinal() >= 60)
+            {
+                Console.WriteLine($"NOTA FINAL = {aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture)}");
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine($"NOTA FINAL = {aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture)}");
+                Console.WriteLine("REPROVADO");
+                double pontosFaltantes = (aluno.NotaFinal() - 60) * (-1);
+                Console.WriteLine($"FALTARAM {pontosFaltantes.ToString("F2", CultureInfo.InvariantCulture)} PONTOS");
+            }
+        }
+    }
+}
